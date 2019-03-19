17106cafbb52f089d731420e7ade45f085448989
toshi-toma <toshitoma1217@gmail.com>
2019-03-20 00:35:12 +0900

    use method and computed, v-on

--- index.js
+++ index.js
@@ -4,5 +4,16 @@ var app = new Vue({
     headerHtml: '<h1>Hello Vue!</h1>',
     message: 'Hello Vue!',
     link: 'https://github.com/vuejs/vue',
+    date: '',
+  },
+  computed: {
+    computedDate: function() {
+      return new Date();
+    }
+  },
+  methods: {
+    methodsDate: function() {
+      this.date =  new Date();
+    }
   }
 })
